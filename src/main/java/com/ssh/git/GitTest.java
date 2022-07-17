a71e1a3c17e4efce039e64c375259e4858ed278e
ssh <1049764221@qq.com>
2022-07-17 23:35:55 +0800

    three commit Master

--- src/main/java/com/ssh/git/GitTest.java
+++ src/main/java/com/ssh/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Master branch");
         System.out.println("Git Master2 branch");
+        System.out.println("Git Master3 branch");
     }
 }
