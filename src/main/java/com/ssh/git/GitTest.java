75d8a19c79680e21b05a3d0eaa07e9c4e56d615b
ssh <1049764221@qq.com>
2022-07-17 23:35:32 +0800

    second commit Master

--- src/main/java/com/ssh/git/GitTest.java
+++ src/main/java/com/ssh/git/GitTest.java
@@ -3,5 +3,6 @@ package com.ssh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Master branch");
+        System.out.println("Git Master2 branch");
     }
 }
